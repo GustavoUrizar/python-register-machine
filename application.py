d84457474e7f75bb89717ac8c2450f2ada623080
Gustavo Urizar <gustavo.urizar>
2015-03-23 12:41:07 -0600

    initial commit funtional code for the first two stories

--- application.py
+++ application.py
@@ -1,3 +1,21 @@
-# Aqui escribe tu codigo
+import time 
+import os
+print """-----------------------------REGISTER MACHINE--------------------------------"""
+print """ Whant do you want to do?"""
 
-print "Hello World"
+
+
+MENU = True
+while MENU == True:
+	OPTION = raw_input(" 1. ADD AN ARTICLE \n 2. SELL ARTICLES \n 3. EXIT  ") 
+	if OPTION == "3":
+		MENU = False
+		print "Thank you for using the Register Machine"
+	elif OPTION == "2":
+		MENU = True
+	elif OPTION == "1":
+		MENU = True
+	else: 
+		print "please insert a valid option"
+	
+	
