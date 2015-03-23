83918be19237888170ca687c936a1fb4f5434b28
Gustavo Urizar <gustavo.urizar>
2015-03-23 15:53:39 -0600

    code is able to insert and save a product

--- application.py
+++ application.py
@@ -3,6 +3,27 @@ import os
 print """-----------------------------REGISTER MACHINE--------------------------------"""
 print """ Whant do you want to do?"""
 
+price = {} 
+article = {}
+Original_list = []
+
+def AddArticle():
+	while True:
+		insert = raw_input("Insert new article")
+		if insert.isalpha():
+			insert.lower()
+			break
+		else:
+			print "please insert a valid article"
+	while True:
+		try:
+			quantity = int(raw_input("insert quantity"))
+			price = float(raw_input("insert Value"))
+			break
+		except ValueError:
+			print "please insert only numbers"
+	article[insert] = quantity
+	price[insert] = price
 
 
 MENU = True
@@ -14,6 +35,7 @@ while MENU == True:
 	elif OPTION == "2":
 		MENU = True
 	elif OPTION == "1":
+		AddArticle()
 		MENU = True
 	else: 
 		print "please insert a valid option"
